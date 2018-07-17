587440ea5f7f2793766ede043c21a6815a226e80
Jose Antonio Bravo Isidro <ja.bravo.isidro@gmail.com>
2018-07-17 22:24:19 +0100

    Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: 'Clifden Lifeboat 10k race',
   },
   pathPrefix: '/test',
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass','gatsby-plugin-netlify-cms'],
 }
